fa2538484141593b738a3031cac863c49cc645d5
chihebgit <chiheb.samti@gmail.com>
2024-11-29 09:42:02 +0100

    init project

---
+++ src/app/dashboardWrapper.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex min-h-screen w-full bg-gray-50 text-gray-900"></div>
+  );
+};
+
+export default DashboardWrapper;
